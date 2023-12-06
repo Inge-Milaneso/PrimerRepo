f921f8e1b8bab30776ff8e7c79eac658f544f834
Inge-Milaneso <aaronsamuuuel@gmail.com>
2023-12-05 19:48:10 -0600

    Se agrega comentario  hacia el profe

--- ProyectoFinalHH.cpp
+++ ProyectoFinalHH.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <limits> // Para limpiar el buffer del cin después de leer un tipo incorrecto
-cout << "Mora es GOD" << endl;
+cout << "Aaron es chido" << endl;
 using namespace std;
 int main() 
 {
